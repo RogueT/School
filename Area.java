f082894126d570e6556dc63400340dc66b178160
RogueT <47229872+RogueT@users.noreply.github.com>
2019-02-11 22:13:59 -0700

    Create Area.java

---
+++ Area.java
@@ -0,0 +1,11 @@
+public class Area {
+    public static void main(String[] args) {
+        
+        double width = 4.5;
+        double height = 7.9;
+        double area = width * height;
+        double perm = ((width * 2)+(height * 2));
+
+        System.out.println("The Area is "+ area +" and the perimeter is "+ perm);
+    }
+}
